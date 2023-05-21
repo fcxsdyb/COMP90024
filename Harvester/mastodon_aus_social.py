84b208cf28cfd3ed1314fb05611deae278ad8c64
fengjitao <1239122714@qq.com>
2023-05-22 03:25:53 +1000

    add some comments

--- Harvester/mastodon_aus_social.py
+++ Harvester/mastodon_aus_social.py
@@ -8,22 +8,23 @@ import re
 
 from textblob import TextBlob
 
-# connect to the couchdb
+# couchdb login data
 admin = 'cccadmin'
 password = 'whysohard24!'
 
-# send request to couchdb
+# connect to couchdb
 url = f'http://{admin}:{password}@172.26.135.17:5984/'
 couch = couchdb.Server(url)
 
-# create a couchdb database called 'mastodon_au_social', if the database exists,just find that database. If not, just creat the database.
+# create a couchdb database called 'mastodon_au_social_final_with_emotion',
+# if the database exists,just find that database. If not, just creat the database.
 db_name = 'mastodon_au_social_final_with_emotion'
 if db_name not in couch:
     db = couch.create(db_name)
 else:
     db = couch[db_name]
 
-
+# mastodon message (with url and private token)
 m = Mastodon(
     api_base_url='https://aus.social',
     access_token='2nwIHJnHrDkZkFnzbFEgDZmsYTTgu8aXnutLRRgermg'
@@ -34,35 +35,42 @@ m = Mastodon(
 class Listener(StreamListener):
     def on_update(self, status):
         # if some message have some error, just print error to let the script run
+        try:
+            # load json element
+            json_element = json.dumps(status, indent=2, sort_keys=True, default=str)
+            json_single = json.loads(json_element)
 
-        json_element = json.dumps(status, indent=2, sort_keys=True, default=str)
-        json_single = json.loads(json_element)
+            #ignore the http tag in the sentence, to extract the real words in the note.
+            no_tags_string = re.sub('<.*?>', '', json_single['content'])
 
-        #ignore the http tag in the sentence, to extract the real words in the note.
-        no_tags_string = re.sub('<.*?>', '', json_single['content'])
+            # Replace Unicode line separator and paragraph separator characters
+            no_special_chars_string = no_tags_string.replace(u'\u2028', ' ').replace(u'\u2029', ' ')
 
-        # Replace Unicode line separator and paragraph separator characters
-        no_special_chars_string = no_tags_string.replace(u'\u2028', ' ').replace(u'\u2029', ' ')
+            # Replace HTML entities
+            readable_string = no_special_chars_string.replace('&gt;', '>')
 
-        # Replace HTML entities
-        readable_string = no_special_chars_string.replace('&gt;', '>')
+            # create a new dictionary to store the capture data
+            new_store = {}
+            new_store['id'] = json_single['account']['id']
+            new_store['content'] = readable_string
+            new_store['created_at'] = json_single['created_at']
 
-        # create a new dictionary to store the capture data
-        new_store = {}
-        new_store['id'] = json_single['account']['id']
-        new_store['content'] = readable_string
-        new_store['created_at'] = json_single['created_at']
+            # Content analysis of user's toots, judge whether the toot is positive, negative or neutral.
+            analysis = TextBlob(new_store['content'])
+            sentiment = analysis.sentiment.polarity
 
-        analysis = TextBlob(new_store['content'])
-        sentiment = analysis.sentiment.polarity
+            if sentiment > 0:
+                new_store["emotion"] = "Positive"
+            elif sentiment < 0:
+                new_store["emotion"] = "Negative"
+            else:
+                new_store["emotion"] = "Neutral"
 
-        if sentiment > 0:
-            new_store["emotion"] = "Positive"
-        elif sentiment < 0:
-            new_store["emotion"] = "Negative"
-        else:
-            new_store["emotion"] = "Neutral"
-        doc_id, doc_rev = db.save(new_store)
+            # if all data preprocess finish, store the data into couchdb
+            doc_id, doc_rev = db.save(new_store)
+
+        except:
+            print("error")
 
 
 # open the harvest
